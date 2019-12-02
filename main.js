540ee143db270749507101e4d4b8ad645e3e953f
Daniel Fowler <dan.h.fowler@gmail.com>
2019-12-02 15:42:12 -0800

    Ran auto-formater on my code

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("mainHeader").onclick = function() {
-      var colors = ["#ff0000", "#ffff00", "#00ff00", "#00ffff", "#0000ff", "#ff00ff"];
-      this.colorID = this.colorID || 0;
-      this.style.color = colors[this.colorID++%colors.length];
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("mainHeader").onclick = function () {
+    var colors = ["#ff0000", "#ffff00", "#00ff00", "#00ffff", "#0000ff", "#ff00ff"];
+    this.colorID = this.colorID || 0;
+    this.style.color = colors[this.colorID++ % colors.length];
+  }
 })
 
-$( document ).ready(function() {
-  $( "#middle" ).click(function() {
-    $( "#middle" ).fadeOut( "slow" );
+$(document).ready(function () {
+  $("#middle").click(function () {
+    $("#middle").fadeOut("slow");
   });
 });
 
-$( document ).ready(function() {
-  $( "#profileIcon" ).click(function() {
-    $( "#profileIcon" ).fadeOut( "slow" );
+$(document).ready(function () {
+  $("#profileIcon").click(function () {
+    $("#profileIcon").fadeOut("slow");
   });
 });
